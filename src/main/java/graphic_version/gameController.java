ebafa8ff58a284996221cc51fe5d08f74a679971
thaidzuicui <hthai313204@gmail.com>
2023-10-27 14:02:32 +0700

    gameController

--- src/main/java/graphic_version/gameController.java
+++ src/main/java/graphic_version/gameController.java
@@ -1,5 +1,33 @@
 package graphic_version;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class gameController {
-    int x;
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void switchToQuiz(ActionEvent e) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Quiz.fxml"));
+        Stage stage = (Stage)((Node)e.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToHangMan(ActionEvent e) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("HangMan.fxml"));
+        Stage stage = (Stage)((Node)e.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
+
